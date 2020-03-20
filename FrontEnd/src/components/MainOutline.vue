5af664f5e4d19d59f2e4fa42f1af9b836b1b7f8a
arition <aritionkb@gmail.com>
2020-03-20 07:58:48 -0700

    add footer

--- FrontEnd/src/components/MainOutline.vue
+++ FrontEnd/src/components/MainOutline.vue
@@ -1,9 +1,12 @@
 <template>
-  <mu-container class="main d-flex align-items-center">
-      <mu-paper :z-depth="2">
-        <slot></slot>
-      </mu-paper>
-  </mu-container>
+  <div>
+    <mu-container class="main d-flex align-items-center">
+        <mu-paper :z-depth="2">
+          <slot></slot>
+        </mu-paper>
+    </mu-container>
+    <div class="footer">Made by&nbsp;<a href="https://github.com/arition" target="_blank">@arition</a>&nbsp;with ❤️️.</div>
+  </div>
 </template>
 
 <script>
@@ -27,5 +30,19 @@ export default {
 
 .main {
   min-height: 90vh;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: right;
+  width: 100%;
+  padding: 20px;
+  background: #eeeeee;
+  display: none;
+  @media (min-width: 992px) {
+    display: block;
+  }
 }
 </style>
